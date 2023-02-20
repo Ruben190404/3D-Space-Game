5777da4c15e99573ea5620bb826d9a4cec6bd25b
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2023-02-20 15:47:01 +0100

    1st push
    mario galaxy gravity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private float _speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float _Hinput = Input.GetAxis("Horizontal");
+        float _Vinput = Input.GetAxis("Vertical");
+        Vector3 _movement = (transform.right * _Hinput + transform.forward * _Vinput) * _speed;
+        _rb.velocity = new Vector3(_movement.x, _rb.velocity.y, _movement.z);
+    }
+}
